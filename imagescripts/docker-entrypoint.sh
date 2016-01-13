1fd91eb8bca004096f1b28dec0557dbefbcf0263
Steffen Bleul <blacklabelops@itbleul.de>
2016-01-13 14:25:01 +0100

    Setting user email for authentication.

--- imagescripts/docker-entrypoint.sh
+++ imagescripts/docker-entrypoint.sh
@@ -7,11 +7,11 @@ set -e
 if [ -n "${GCLOUD_ACCOUNT}" ]; then
   echo ${GCLOUD_ACCOUNT} >> /opt/gcloud/auth.base64
   base64 -d /opt/gcloud/auth.base64 >> /opt/gcloud/auth.json
-  gcloud auth activate-service-account --key-file=/opt/gcloud/auth.json
+  gcloud auth activate-service-account --key-file=/opt/gcloud/auth.json ${GCLOUD_ACCOUNT_EMAIL}
 fi
 
 if [ -n "${GCLOUD_ACCOUNT_FILE}" ]; then
-  gcloud auth activate-service-account --key-file=${GCLOUD_ACCOUNT_FILE}
+  gcloud auth activate-service-account --key-file=${GCLOUD_ACCOUNT_FILE} ${GCLOUD_ACCOUNT_EMAIL}
 fi
 
 if [ -n "${GCLOUD_CRON}" ]; then
